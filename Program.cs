c64dd1ec81c3a500acd508bd99fa163a08b78c07
JesusTadeo7 <tadeo3534@gmail.com>
2026-05-17 05:56:27 +0000

    fix: conexion postgres y estabilidad EF v3

--- Program.cs
+++ Program.cs
@@ -3,11 +3,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // 🔌 conexión a SQL Server
-var conn = Environment.GetEnvironmentVariable("DATABASE_URL");
-
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(conn, o =>
-        o.EnableRetryOnFailure()
+    options.UseNpgsql(Environment.GetEnvironmentVariable("DATABASE_URL")));
 
 // swagger
 builder.Services.AddEndpointsApiExplorer();
